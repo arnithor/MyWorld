0f4a46259a8865a1506ab1ebb3f9c5359e2799b3
arnithor <arnithor@gmail.com>
2013-02-24 21:55:10 +0000

    Testing GitHub

---
+++ MyWorld/src/MyWorld.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : MyWorld.c
+ Author      :
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	printf("I'm going to share this\n");
+	return EXIT_SUCCESS;
+}
